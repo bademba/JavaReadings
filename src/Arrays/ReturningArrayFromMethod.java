2d513d64a428d437d8b14188b161b7608f8b6bd9
bademba <brian.ademba@gmail.com>
2023-01-18 23:29:34 +0300

    small update

--- src/Arrays/ReturningArrayFromMethod.java
+++ src/Arrays/ReturningArrayFromMethod.java
@@ -10,10 +10,10 @@ public class ReturningArrayFromMethod {
         Scanner input =new Scanner(System.in);
         double[] temperatureOut= new double[7];
         for(int i=0;i<temperatureOut.length;i++){
-            System.out.print("enter max temp for the day: [" +(i+1) +"]");
+            System.out.print("enter max temp for the day [" +(i+1) +"]: ");
             temperatureOut[i] =input.nextDouble();
         }
-        System.out.println(temperatureOut);
+        //System.out.println(temperatureOut);
         return temperatureOut;
     }
 }
