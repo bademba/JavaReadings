3083ca6768a39d576053d508f6bde82b4cabd29f
bademba <brian.ademba@gmail.com>
2023-01-19 18:54:14 +0300

    Array Summation
    1. For loop
    2. Enhanced for loop
    3. Varargs and for loop

---
+++ src/Arrays/ArraySummation.java
@@ -0,0 +1,39 @@
+package Arrays;
+
+public class ArraySummation {
+
+    //runner method
+    public static void main(String[] args) {
+        double[] arrayIn={12.5,13.5};
+        summation1(arrayIn);
+        summation2(arrayIn);
+        summation3(arrayIn);
+    }
+    //using for loop
+    static double summation1(double[] arrayIn){
+        double sum=0;
+        for(int i=0;i<arrayIn.length;i++){
+            sum=sum + arrayIn[i];
+        }
+        System.out.println("SUMMATION 1:" +sum);
+        return sum;
+    }
+    //using enhanced loop
+    static double summation2(double[] arr){
+        double sum=0;
+        for(double item: arr){
+            sum=sum+item;
+        }
+        System.out.println("SUMMATION 2:" +sum);
+        return sum;
+    }
+    //usind varargs and for loop
+    static double summation3(double... arr){
+       double sum=0;
+       for(int i =0;i<arr.length;i++){
+           sum=sum+arr[i];
+       }
+        System.out.println("SUMMATION 3:" +sum);
+        return sum;
+    }
+}
