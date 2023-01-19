a70f4b56d55ca506f854eb6603a41caab1b4defe
bademba <brian.ademba@gmail.com>
2023-01-19 18:35:11 +0300

    Added pseudocode

--- src/Arrays/ArrayMaximum.java
+++ src/Arrays/ArrayMaximum.java
@@ -8,6 +8,16 @@ public class ArrayMaximum {
         int[] array = {22222222,34444,400000};
         max(array);
     }
+
+//    SET result TO first value in array
+//    LOOP FROM second element in array TO last element in array
+//            BEGIN
+//    IF current element > result
+//            BEGIN
+//    SET result TO current element
+//            END
+//    END
+//    RETURN result
     static int max(int[] arrayIn){
         int result =arrayIn[0];
         for(int i=1;i<arrayIn.length;i++){
