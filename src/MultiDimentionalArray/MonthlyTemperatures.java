fa198c1819a3391a673eb407211878555cb7274a
Brian Ademba <brian.ademba@cellulant.io>
2023-01-20 21:38:45 +0300

    multidimensional array

---
+++ src/MultiDimentionalArray/MonthlyTemperatures.java
@@ -0,0 +1,11 @@
+package MultiDimentionalArray;
+
+import java.util.Scanner;
+
+public class MonthlyTemperatures {
+    public static void main(String[] args) {
+        Scanner keyboard = new Scanner(System.in);
+        char choice;
+        double[][] temperature = new double[4][7];
+    }
+}
