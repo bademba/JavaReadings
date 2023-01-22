6bd68b2d612a643833434b5ec525c38b3a184975
bademba <brian.ademba@gmail.com>
2023-01-22 20:15:31 +0300

    added displayWeek method

--- src/MultiDimentionalArray/MonthlyTemperatures.java
+++ src/MultiDimentionalArray/MonthlyTemperatures.java
@@ -21,8 +21,8 @@ public class MonthlyTemperatures {
                         break;
                 case'2': displayAllTemps(temperature);
                         break;
-//                case'3': displayWeek(temperature);
-//                        break;
+                case'3': displayWeek(temperature);
+                        break;
 //                case'4': displayDays(temperature);
 //                        break;
 //                case'5': System.out.println("Goodbye");
@@ -46,7 +46,7 @@ public class MonthlyTemperatures {
         }
     }
 
-    //method to display  all temperatures in 2D array reuires a nested loop
+    //method to display  all temperatures in 2D array requires a nested loop
     static void displayAllTemps(double[][] temepartureIn){
         System.out.println();
         System.out.println("***TEMPERATURES ENTERED****");
@@ -54,10 +54,32 @@ public class MonthlyTemperatures {
         for (int week=1; week<=temepartureIn.length;week++){
             //inner loop control day number
             for (int day=1;day<=temepartureIn[0].length;day++){
-                System.out.println("week "+week +" day " +day +temepartureIn[week-1][day-1]);
+                System.out.println("week "+week +" day " +" "+day +temepartureIn[week-1][day-1]);
             }
         }
 
+    }
+
+    //method to display temeperature for a single week requires a single loop
+    static void displayWeek(double[][] temperatureIn){
+        Scanner keyboard = new Scanner(System.in);
+        int week;
+        System.out.println("Enter week number (1-4): ");
+        week=keyboard.nextInt();
+        //input validation : week should be between 1 and 4
+        while(week<1 || week>4){
+            System.out.println("Invalid week number");
+            System.out.println("Enter again (1-4 only): ");
+            week=keyboard.nextInt();
+        }
+        //display temperature for given week
+        System.out.println();
+        System.out.println("*** TEMPERATURE ENTERED FOR WEEK "+week+" ***");
+        System.out.println();
+        //week number is fixed so loop required to process day number only
+        for(int day=1;day<=temperatureIn[0].length;day++){
+            System.out.println("week "+week+" day "+day+": "+temperatureIn[week-1][day-1]);
+        }
     }
 
 }
