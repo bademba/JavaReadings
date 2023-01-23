961f1628bc458da5f26234b6933ee60f662e10fa
bademba <brian.ademba@gmail.com>
2023-01-23 22:57:55 +0300

    Adding tests

---
+++ src/Arrays/Test/TestArrays.java
@@ -0,0 +1,14 @@
+package Arrays.Test;
+
+public class TestArrays {
+    public static void main(String[] args) {
+        //print the length of the array
+        SomeArray();
+    }
+
+    static void SomeArray(){
+        int[] someArray={2,5,1,9,11};
+        System.out.println(someArray.length);
+
+    }
+}
