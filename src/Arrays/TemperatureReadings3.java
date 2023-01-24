e3a06971ab93cc006b219961f980d996fc213ce0
Brian Ademba <brian.ademba@cellulant.io>
2023-01-24 14:46:52 +0300

    Added  wasHot days

--- src/Arrays/TemperatureReadings3.java
+++ src/Arrays/TemperatureReadings3.java
@@ -2,12 +2,13 @@ package Arrays;
 
 import java.util.Scanner;
 
-public class TemperatureReadings {
+public class TemperatureReadings3 {
     public static void main(String[] args) {
         double[] temperature = new double[7];
-
         enterTemps(temperature);
         displayTemps(temperature);
+        wasHot(temperature);
+        wasHotUsingEnhancedLoop(temperature);
     }
 
     //method to enter temperaures
@@ -26,6 +27,29 @@ public class TemperatureReadings {
         System.out.println();
         for (int i=0;i<temperatureIn.length;i++){
             System.out.println("day " +(1+i)+" "+temperatureIn[i]);
+        }
+    }
+
+    //wasHot method using for loop
+    static void wasHot(double[] temperatureIn){
+        System.out.println();
+        System.out.println("**HOT DAYS***");
+        System.out.println();
+        for (int i =0;i<temperatureIn.length;i++){
+            if(temperatureIn[i]>18){
+                System.out.println(temperatureIn[i]);
+            }
+        }
+    }
+
+    //wasHot using enhanced loop
+    static void wasHotUsingEnhancedLoop(double[] temperatureIn){
+        System.out.println();
+        System.out.println("**HOT DAYS USING enhanced Loop***");
+        System.out.println();
+        for(double item:temperatureIn){
+            if(item>18){
+                System.out.println(item);}
         }
     }
 }
