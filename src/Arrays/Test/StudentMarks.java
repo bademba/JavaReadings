a9a5f6678899bc9d0d38b0d1093c793a089b337c
Brian Ademba <brian.ademba@cellulant.io>
2023-01-26 23:35:53 +0300

    more code

--- src/Arrays/Test/StudentMarks.java
+++ src/Arrays/Test/StudentMarks.java
@@ -5,18 +5,27 @@ import java.util.Scanner;
 public class StudentMarks {
     public static void main(String[] args) {
         int[] javaStudents=new int[3];
-        enterExamMarks();
+        //enterExamMarks();
+        enterExamMarks2(javaStudents);
         increaseMarks(javaStudents);
     }
 
-    static int[] enterExamMarks(){
+//    static int[] enterExamMarks(){
+//        Scanner keyboard = new Scanner(System.in);
+//        int[] marksOut =new int[3];
+//        for (int i=0;i<marksOut.length;i++){
+//             System.out.print("Enter marks [" +"] : ");
+//            marksOut[i]=keyboard.nextInt();
+//         }
+//         return marksOut;
+//    }
+
+    static void enterExamMarks2(int[] marks){
         Scanner keyboard = new Scanner(System.in);
-        int[] marksOut =new int[3];
-        for (int i=0;i<marksOut.length;i++){
-             System.out.print("Enter marks [" +"] : ");
-            marksOut[i]=keyboard.nextInt();
-         }
-         return marksOut;
+        for(int i=0;i<marks.length;i++){
+            System.out.print("Enter marks ["+ i+"] : ");
+            marks[i]=keyboard.nextInt();
+        }
     }
 
     static  void increaseMarks(int[] marks){
@@ -24,7 +33,8 @@ public class StudentMarks {
         System.out.println("***INCREASED MARKS***");
         for (int i=0;i< marks.length;i++){
             marks[i]=marks[i]+5;
-            System.out.println("Marks after increase: "+marks[i]);
+            System.out.println(marks[i]);
+           // System.out.println("Marks after increase: "+(marks[i]+5));
         }
 
     }
