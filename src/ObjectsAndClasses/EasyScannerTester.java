94d31738031fa59080710d6547b3d04fe4ea98ea
Brian Ademba <brian.ademba@cellulant.io>
2023-01-31 18:13:58 +0300

    EasyScanner

---
+++ src/ObjectsAndClasses/EasyScannerTester.java
@@ -0,0 +1,8 @@
+package ObjectsAndClasses;
+
+public class EasyScannerTester {
+    public static void main(String[] args) {
+        System.out.print("Enter double:");
+        double d= EasyScanner.nextDouble();
+    }
+}
