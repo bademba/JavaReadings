7b9ef656cedb48be99cbe145bc6c6f25231eae1a
Brian Ademba <brian.ademba@cellulant.io>
2023-01-31 18:29:37 +0300

    ConsoleTester: Did not run

--- src/ObjectsAndClasses/EasyScannerTester.java
+++ src/ObjectsAndClasses/EasyScannerTester.java
@@ -4,5 +4,6 @@ public class EasyScannerTester {
     public static void main(String[] args) {
         System.out.print("Enter double:");
         double d= EasyScanner.nextDouble();
+        System.out.println("You entered: "+d);
     }
 }
